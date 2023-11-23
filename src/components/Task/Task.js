d9913401ae2641e3d29881fcc96e86d3da37d992
Honor <nik26sych@gmail.com>
2023-11-23 18:19:41 +0300

    начало проекта

---
+++ src/components/Task/Task.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Task() {
+  return (
+    <p>re</p>
+  );
+}
+
+export default Task;
---
+++ src/components/Tasklist/Tasklist.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Task from '../Task/Task';
+
+function Tasklist() {
+  return (
+    <Task />
+  );
+}
+
+export default Tasklist;
